81f2de403541d36ec06948031b64b6c3704d20f6
belven <belven000@gmail.com>
2014-08-14 20:26:36 +0100

    Fixed an issue with the hunter giving items that don't stack more than 1.

--- src/belven/professions/Hunter.java
+++ src/belven/professions/Hunter.java
@@ -7,27 +7,24 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class Hunter extends Profession
-{
-    public Hunter(Player currentPlayer, ProfessionManager instance)
-    {
-        pOwner = currentPlayer;
-        plugin = instance;
-    }
+public class Hunter extends Profession {
+	public Hunter(Player currentPlayer, ProfessionManager instance) {
+		pOwner = currentPlayer;
+		plugin = instance;
+	}
 
-    public void EntityDeathEvent(EntityDeathEvent event)
-    {
-        Random rand = new Random();
+	public void EntityDeathEvent(EntityDeathEvent event) {
+		Random rand = new Random();
 
-        Iterator<ItemStack> eventDrops = event.getDrops().iterator();
+		Iterator<ItemStack> eventDrops = event.getDrops().iterator();
 
-        while (eventDrops.hasNext())
-        {
-            int ran = rand.nextInt(3);
-            ran++;
-            ItemStack is = eventDrops.next();
-            is.setAmount(is.getAmount() + ran);
-        }
-
-    }
+		while (eventDrops.hasNext()) {
+			int ran = rand.nextInt(3);
+			ran++;
+			ItemStack is = eventDrops.next();
+			if (is.getMaxStackSize() != 1) {
+				is.setAmount(is.getAmount() + ran);
+			}
+		}
+	}
 }
